ee1e568c5ef9188dc26a76ce2714c6cd4a5daa77
Zuyao Chen <zuychen@clariden-ln002.cscs.ch>
2025-04-04 20:46:19 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 
 
 
-pip install torch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+
 
 pip install --upgrade --no-build-isolation flash-attn==2.7.4.post1
 
