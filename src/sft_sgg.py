1656923c79421abe721c7eea507df4f77aeb0b9c
Zuyao Chen <zuychen@eu-login-31.euler.ethz.ch>
2025-04-07 22:26:52 +0200

    set use_cache=False for sft_sgg.py

--- src/sft_sgg.py
+++ src/sft_sgg.py
@@ -192,7 +192,7 @@ def main():
         trust_remote_code=model_args.trust_remote_code,
         attn_implementation=model_args.attn_implementation,
         torch_dtype=model_args.torch_dtype,
-        use_cache=False if training_args.gradient_checkpointing else True,
+        use_cache=False, #if training_args.gradient_checkpointing else True,
         device_map=get_kbit_device_map() if quantization_config is not None else None,
         quantization_config=quantization_config,
     )
