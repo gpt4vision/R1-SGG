eea75a88fb83d5fe76a5888e61e13a96c1284b45
Zuyao Chen <zuychen@eu-login-12.euler.ethz.ch>
2025-04-12 11:05:40 +0200

    debug

--- open_r1/trainer/grpo_trainer.py
+++ open_r1/trainer/grpo_trainer.py
@@ -33,7 +33,7 @@ from contextlib import nullcontext
 from typing import Any, Callable, Optional, Sized, Union
 
 import torch
-import torch.utils.data
+from torch.utils.data import DataLoader
 import transformers
 from accelerate.utils import broadcast_object_list, gather, gather_object, is_peft_model, set_seed
 
@@ -486,7 +486,6 @@ class GRPOTrainerV2(Trainer):
             )        
         
         assert args.custom_per_device_train_batch_size > 0, "please set custom_per_device_train_batch_size > 0!"
-        args.per_device_train_batch_size = args.custom_per_device_train_batch_size * args.gradient_accumulation_steps # trick: we just gather all inputs one time for accumulation
 
         super().__init__(
             model=model,
@@ -629,6 +628,42 @@ class GRPOTrainerV2(Trainer):
         # Instead, we set them to the columns expected by the `training_step` method, hence the override.
         if self._signature_columns is None:
             self._signature_columns = ["prompt"]
+
+    def get_train_dataloader(self) -> DataLoader:
+        """
+        Returns the training [`~torch.utils.data.DataLoader`].
+
+        Will use no sampler if `train_dataset` does not implement `__len__`, a random sampler (adapted to distributed
+        training if necessary) otherwise.
+
+        Subclass and override this method if you want to inject some custom behavior.
+        """
+        if self.train_dataset is None:
+            raise ValueError("Trainer: training requires a train_dataset.")
+
+        train_dataset = self.train_dataset
+        data_collator = self.data_collator
+        if is_datasets_available() and isinstance(train_dataset, datasets.Dataset):
+            train_dataset = self._remove_unused_columns(train_dataset, description="training")
+        else:
+            data_collator = self._get_collator_with_removed_columns(data_collator, description="training")
+
+        dataloader_params = {
+            "batch_size": self._train_batch_size,
+            "collate_fn": data_collator,
+            "num_workers": self.args.dataloader_num_workers,
+            "pin_memory": self.args.dataloader_pin_memory,
+            "persistent_workers": self.args.dataloader_persistent_workers,
+        }
+        print("_train_batch_size:", self._train_batch_size)
+
+        if not isinstance(train_dataset, torch.utils.data.IterableDataset):
+            dataloader_params["sampler"] = self._get_train_sampler()
+            dataloader_params["drop_last"] = self.args.dataloader_drop_last
+            dataloader_params["worker_init_fn"] = seed_worker
+            dataloader_params["prefetch_factor"] = self.args.dataloader_prefetch_factor
+
+        return self.accelerator.prepare(DataLoader(train_dataset, **dataloader_params))            
 
     def _get_train_sampler(self) -> Sampler:
         # Returns a sampler that
